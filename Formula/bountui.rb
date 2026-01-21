00b2cfc800f1febabcc1bcbed0eb6b4c0724c8a1
Cedric Krause <cedware-dev@protonmail.com>
2026-01-21 09:16:07 +0000

    Updated bountui formula to version

--- Formula/bountui.rb
+++ Formula/bountui.rb
@@ -1,8 +1,8 @@
 class Bountui < Formula
   desc "A boundary terminal ui"
   homepage "https://github.com/Cedware/bountui"
-  url "https://github.com/Cedware/bountui/releases/download/v0.7.0/bountui-0.7.0-aarch64-apple-darwin.zip"
-  sha256 "2a2422ccd0ac54013bce100992e8974ac24517c17707664e2556510f290b2c62"
+  url "https://github.com/Cedware/bountui/releases/download/v0.8.0/bountui-0.8.0-aarch64-apple-darwin.zip"
+  sha256 "4fa3bac1cccd9f0a3f516a95fc9e111e2fc14670f456492b7bdb20628855d17b"
   license "GPL-3.0"
 
   depends_on "hashicorp/tap/boundary"
